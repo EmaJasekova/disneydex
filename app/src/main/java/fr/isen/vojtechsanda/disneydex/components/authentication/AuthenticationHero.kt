46050f9ea9ccaa4a8778ea1bcfc230baf988c485
MinotaurTheGreat <134445612+minotavi11@users.noreply.github.com>
2026-03-09 15:20:21 +0100

    feat: Created the UI for Authentication

--- app/src/main/java/fr/isen/vojtechsanda/disneydex/components/authentication/AuthenticationHero.kt
+++ app/src/main/java/fr/isen/vojtechsanda/disneydex/components/authentication/AuthenticationHero.kt
@@ -2,14 +2,20 @@ package fr.isen.vojtechsanda.disneydex.components.authentication
 
 import android.R.id.message
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,13 +29,27 @@ fun AuthenticationHero(modifier: Modifier = Modifier) {
     Box(modifier) {
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier .fillMaxWidth()
+        )
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+                .background(brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        Color(0xFF333332)
+                    )
+                ))
         )
         Text(
             text = "DisneyDex",
-            fontSize = 36.sp,
-            color = MaterialTheme.colorScheme.secondary,
-            modifier = Modifier.padding(bottom = 32.dp)
+            fontSize = 50.sp,
+            color = Color.White,
+            modifier = Modifier
+                .padding(bottom = 72.dp)
+                .align(Alignment.BottomCenter)
         )
     }
 }
