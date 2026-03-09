07d4c6bd4a13813057b0202420ec698cb5b501f5
MinotaurTheGreat <134445612+minotavi11@users.noreply.github.com>
2026-03-09 17:00:50 +0100

    fix: Adjust the code formatting, comply to the standard comment practices, adjust sizes and values, polish UI

--- app/src/main/java/fr/isen/vojtechsanda/disneydex/screens/loginScreen/LoginScreen.kt
+++ app/src/main/java/fr/isen/vojtechsanda/disneydex/screens/loginScreen/LoginScreen.kt
@@ -1,43 +1,31 @@
 package fr.isen.vojtechsanda.disneydex.screens.loginScreen
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.font.FontWeight
-import fr.isen.vojtechsanda.disneydex.components.AppHeader
 import fr.isen.vojtechsanda.disneydex.components.authentication.AuthenticationHero
 import fr.isen.vojtechsanda.disneydex.screens.loginScreen.components.LoginForm
-import fr.isen.vojtechsanda.disneydex.screens.registerScreen.components.RegisterForm
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize()
     ) {
         // Top Half (Hero Section)
         Box(
-            modifier = Modifier
+            modifier = modifier
                 .fillMaxWidth()
-                .weight(1f),
+                .weight(1.4f),
         ) {
             AuthenticationHero()
         }
-
         // Bottom Half (Form Section)
         Column(
-            modifier = Modifier
+            modifier = modifier
                 .fillMaxWidth()
-                .weight(2.8f)
-                .padding(top = 40.dp, start = 24.dp, end = 24.dp, bottom = 10.dp),
+                .weight(2.6f)
+                .padding(top = 40.dp, start = 24.dp, end = 24.dp, bottom = 12.dp),
         ) {
             LoginForm()
         }
