7476bc177e04d214a21a127e5001a8d411c5ebea
EmaJasekova <xjasekov@fi.muni.cz>
2026-03-09 13:59:37 +0100

    feat: add dto to domain object mappers

---
+++ app/src/main/java/fr/isen/vojtechsanda/disneydex/mappers/Mappers.kt
@@ -0,0 +1,39 @@
+package fr.isen.vojtechsanda.disneydex.mappers
+
+import fr.isen.vojtechsanda.disneydex.dto.MovieDto
+import fr.isen.vojtechsanda.disneydex.dto.SagaDto
+import fr.isen.vojtechsanda.disneydex.dto.UniverseDto
+import fr.isen.vojtechsanda.disneydex.dto.UniversesDto
+import fr.isen.vojtechsanda.disneydex.domain.Movie
+import fr.isen.vojtechsanda.disneydex.domain.Saga
+import fr.isen.vojtechsanda.disneydex.domain.Universe
+import fr.isen.vojtechsanda.disneydex.domain.Universes
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+private val DB_DATE_FORMAT: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE
+
+object Mappers {
+
+    fun toMovie(dto: MovieDto): Movie = Movie(
+        name = dto.name,
+        releaseDate = runCatching {
+            LocalDate.parse(dto.release_date, DB_DATE_FORMAT)
+        }.getOrNull()
+    )
+
+    fun toSaga(dto: SagaDto): Saga = Saga(
+        name = dto.name,
+        genre = dto.genre,
+        movies = dto.movies.map(::toMovie)
+    )
+
+    fun toUniverse(dto: UniverseDto): Universe = Universe(
+        name = dto.name,
+        sagas = dto.sagas.map(::toSaga)
+    )
+
+    fun toUniverses(dto: UniversesDto): Universes = Universes(
+        universes = dto.universes.map(::toUniverse)
+    )
+}
