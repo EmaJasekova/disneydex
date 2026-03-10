a0741f9b3dac97954e239a2d814292988ac19f7f
EmaJasekova <xjasekov@fi.muni.cz>
2026-03-10 15:47:40 +0100

    feat: add logout functionality

--- app/src/main/java/fr/isen/vojtechsanda/disneydex/domain/model/AuthContext.kt
+++ app/src/main/java/fr/isen/vojtechsanda/disneydex/domain/model/AuthContext.kt
@@ -2,5 +2,6 @@ package fr.isen.vojtechsanda.disneydex.domain.model
 
 enum class AuthContext(val fallbackMessage: String) {
     LOGIN("Login failed. Please try again."),
-    REGISTER("Registration failed. Please try again.")
+    REGISTER("Registration failed. Please try again."),
+    LOGOUT("Logout failed. Please try again.")
 }
