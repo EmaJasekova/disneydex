83da939147b4410e6b859b754cf57cca081954c3
MinotaurTheGreat <134445612+minotavi11@users.noreply.github.com>
2026-03-10 14:23:31 +0100

    feat: Create UI for UniversesScreen with dynamic components

--- app/src/main/java/fr/isen/vojtechsanda/disneydex/ui/screens/universesScreen/UniversesScreen.kt
+++ app/src/main/java/fr/isen/vojtechsanda/disneydex/ui/screens/universesScreen/UniversesScreen.kt
@@ -1,16 +1,53 @@
 package fr.isen.vojtechsanda.disneydex.ui.screens.universesScreen
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import fr.isen.vojtechsanda.disneydex.R
 import fr.isen.vojtechsanda.disneydex.ui.components.layout.AuthedScaffold
+import fr.isen.vojtechsanda.disneydex.ui.screens.universesScreen.components.UniverseCard
 
 @Composable
 fun UniversesScreen(navController: NavHostController) {
-    AuthedScaffold(
-        navController = navController,
-        content = {
-            Text("This is list of universes")
+    AuthedScaffold( navController = navController,
+    content = {
+
+        Column() {
+
+            UniverseCard(
+                title = "Marvel Cinematic",
+                subtitle = "Phase 1 - Phase 5",
+                posterImages = listOf(R.drawable.poster1, R.drawable.poster2, R.drawable.poster3),
+                count = 12
+            )
+
+            UniverseCard(
+                title = "Marvel Cinematic",
+                subtitle = "Phase 1 - Phase 5",
+                posterImages = listOf(R.drawable.poster1, R.drawable.poster2, R.drawable.poster3),
+                count = 12
+            )
+
+            UniverseCard(
+                title = "Marvel Cinematic",
+                subtitle = "Phase 1 - Phase 5",
+                posterImages = listOf(R.drawable.poster1, R.drawable.poster2, R.drawable.poster3),
+                count = 12
+            )
         }
-    )
-}
+    }
+        )
+
+    }
+
+
+// Usage Example
+
+//        UniverseCard(
+//            title = "Marvel Cinematic",
+//            subtitle = "Phase 1 - Phase 5",
+//            badge = "New",
+//            posterImages = listOf(R.drawable.poster1, R.drawable.poster2, R.drawable.poster3),
+//            memberCount = 42,
+//            onClick = { /* navigate */ }
+//        )
