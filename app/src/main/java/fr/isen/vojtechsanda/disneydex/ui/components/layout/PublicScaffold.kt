3d6311f384b7c47b12018c75b45b1214efbf7f94
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2026-03-10 15:31:21 +0100

    feat: Add unified padding and scrolling to public scaffold

--- app/src/main/java/fr/isen/vojtechsanda/disneydex/ui/components/layout/PublicScaffold.kt
+++ app/src/main/java/fr/isen/vojtechsanda/disneydex/ui/components/layout/PublicScaffold.kt
@@ -1,24 +1,47 @@
 package fr.isen.vojtechsanda.disneydex.ui.components.layout
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import fr.isen.vojtechsanda.disneydex.ui.theme.AppBackgroundColor
 
 @Composable
 fun PublicScaffold(
-    content: @Composable (modifier: Modifier) -> Unit,
+    content: @Composable () -> Unit,
+    hero: @Composable () -> Unit = {},
 ) {
+
     Scaffold(
         containerColor = AppBackgroundColor,
         modifier = Modifier.fillMaxSize()
     ) { innerPadding ->
-        content(
-            Modifier.padding(
-                bottom = innerPadding.calculateBottomPadding()
-            )
-        )
+        val scrollState = rememberScrollState()
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(scrollState)
+        ) {
+            hero()
+
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .padding(
+                        top = 0.dp,
+                        bottom = innerPadding.calculateBottomPadding(),
+                        start = 24.dp,
+                        end = 24.dp,
+                    )
+            ) {
+                content()
+            }
+        }
     }
 }
