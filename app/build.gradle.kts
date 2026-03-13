7fc0c71f47cc517b21de7491034bda6d63cce8f3
MinotaurTheGreat <134445612+minotavi11@users.noreply.github.com>
2026-03-13 10:47:31 +0100

    chore: Fix libs.versions.toml sync

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,5 +68,5 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.material.icons.extended)
-    implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation(libs.coil.compose)
 }
