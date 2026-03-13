b31657894390abdbcfc5cc4e65163ff59c4c145c
EmaJasekova <xjasekov@fi.muni.cz>
2026-03-13 09:38:20 +0100

    fix: add AuthUserDto

--- app/src/main/java/fr/isen/vojtechsanda/disneydex/domain/model/AuthUser.kt
+++ app/src/main/java/fr/isen/vojtechsanda/disneydex/domain/model/AuthUser.kt
@@ -2,6 +2,6 @@ package fr.isen.vojtechsanda.disneydex.domain.model
 
 data class AuthUser(
     val id: String,
-    val email: String?,
-    val username: String?
+    val email: String,
+    val username: String
 )
