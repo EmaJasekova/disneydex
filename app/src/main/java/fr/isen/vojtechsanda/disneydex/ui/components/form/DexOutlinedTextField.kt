826ca31e59e0ad3b156ae2db81fc74266937bfc4
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2026-03-20 02:10:34 +0100

    feat: Improve user UI

--- app/src/main/java/fr/isen/vojtechsanda/disneydex/ui/components/form/DexOutlinedTextField.kt
+++ app/src/main/java/fr/isen/vojtechsanda/disneydex/ui/components/form/DexOutlinedTextField.kt
@@ -1,13 +1,17 @@
 package fr.isen.vojtechsanda.disneydex.ui.components.form
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -24,7 +28,20 @@ fun DexOutlinedTextField(
     isPassword: Boolean = keyboardType == KeyboardType.Password,
     isError: Boolean = false,
     leadingIcon: @Composable (() -> Unit)? = null,
-    shape: Shape = RoundedCornerShape(10.dp)
+    shape: Shape = RoundedCornerShape(10.dp),
+    singleLine: Boolean = false,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    placeholder: String? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    prefix: @Composable (() -> Unit)? = null,
+    suffix: @Composable (() -> Unit)? = null,
+    supportingText: @Composable (() -> Unit)? = null,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
+    minLines: Int = 1,
+    interactionSource: MutableInteractionSource? = null,
 ) {
     OutlinedTextField(
         label = label?.let { { Text(it) } },
@@ -37,6 +54,18 @@ fun DexOutlinedTextField(
         singleLine = true,
         shape = shape,
         isError = isError,
-        leadingIcon = leadingIcon
+        leadingIcon = leadingIcon,
+        enabled = enabled,
+        readOnly = readOnly,
+        textStyle = textStyle,
+        placeholder = placeholder?.let { { Text(it) } },
+        trailingIcon = trailingIcon,
+        prefix = prefix,
+        suffix = suffix,
+        supportingText = supportingText,
+        keyboardActions = keyboardActions,
+        maxLines = maxLines,
+        minLines = minLines,
+        interactionSource = interactionSource,
     )
 }
