bad336994ea28935632578b2970900411060d1a1
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2026-03-21 23:44:46 +0100

    fix: Add error catching to ObserveMovieTradersUseCase

--- app/src/main/java/fr/isen/vojtechsanda/disneydex/domain/usecase/movie/ObserveMovieTradersUseCase.kt
+++ app/src/main/java/fr/isen/vojtechsanda/disneydex/domain/usecase/movie/ObserveMovieTradersUseCase.kt
@@ -3,14 +3,18 @@ package fr.isen.vojtechsanda.disneydex.domain.usecase.movie
 import fr.isen.vojtechsanda.disneydex.domain.model.User
 import fr.isen.vojtechsanda.disneydex.domain.repository.MovieRepository
 import fr.isen.vojtechsanda.disneydex.domain.repository.UserRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flatMapLatest
 
 class ObserveMovieTradersUseCase(
     private val movieRepository: MovieRepository,
     private val userRepository: UserRepository
 ) {
+    @OptIn(ExperimentalCoroutinesApi::class)
     operator fun invoke(movieId: String): Flow<Result<List<User?>>> =
         movieRepository.observeMovieTraders(movieId)
             .flatMapLatest { uids -> userRepository.observeUsers(uids) }
+            .catch { e -> emit(Result.failure(e)) }
 }
