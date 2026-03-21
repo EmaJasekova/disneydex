71dc0f2e533b54c0cb9d34c6ac897fd0fe2708c4
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2026-03-21 23:48:04 +0100

    feat: Connect movie community card to BE

--- app/src/main/java/fr/isen/vojtechsanda/disneydex/ui/screens/movieDetailScreen/components/communityCard/CommunityCard.kt
+++ app/src/main/java/fr/isen/vojtechsanda/disneydex/ui/screens/movieDetailScreen/components/communityCard/CommunityCard.kt
@@ -4,30 +4,33 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import fr.isen.vojtechsanda.disneydex.domain.model.Movie
-import fr.isen.vojtechsanda.disneydex.domain.model.User
+import fr.isen.vojtechsanda.disneydex.ui.components.common.DexLoader
 import fr.isen.vojtechsanda.disneydex.ui.screens.movieDetailScreen.components.DetailContainer
 
 @Composable
-fun CommunityCard(movie: Movie) {
-
-    val users = listOf(
-        User(uid = "user-1", email = "test-1@idk.idk", username = "Tester 1"),
-        User(uid = "user-2", email = "test-2@idk.idk", username = "Tester 2"),
-        User(uid = "user-3", email = "test-3@idk.idk", username = "Tester 3"),
-    )
+fun CommunityCard(movie: Movie, viewModel: CommunityCardViewModel = viewModel()) {
+    val movieTradersState by viewModel.movieTraders.collectAsState()
 
     DetailContainer(title = "Available from community") {
         Column(
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            if (users.size == 0) {
-                Text(text = "There are no copies of this movie available.", color = Color.LightGray)
-            }
+            DexLoader(movieTradersState) { traders ->
+                if (traders.isEmpty()) {
+                    Text(
+                        text = "There are no copies of this movie available.",
+                        color = Color.LightGray
+                    )
+                }
 
-            users.map { user -> CommunityUserCard(user, movie) }
+                traders.map { trader -> CommunityUserCard(trader, movie) }
+            }
         }
     }
 }
