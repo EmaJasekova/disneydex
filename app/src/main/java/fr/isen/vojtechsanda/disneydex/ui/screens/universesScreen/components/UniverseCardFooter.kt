a8d541a7b9ccf72ad5924d34f46c2e0794c64b7a
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2026-03-21 22:13:24 +0100

    chore: Remove movie traders preview from universe card

--- app/src/main/java/fr/isen/vojtechsanda/disneydex/ui/screens/universesScreen/components/UniverseCardFooter.kt
+++ app/src/main/java/fr/isen/vojtechsanda/disneydex/ui/screens/universesScreen/components/UniverseCardFooter.kt
@@ -3,33 +3,26 @@ package fr.isen.vojtechsanda.disneydex.ui.screens.universesScreen.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import fr.isen.vojtechsanda.disneydex.domain.model.User
 import fr.isen.vojtechsanda.disneydex.ui.components.common.DexButton
 
 @Composable
-fun UniverseCardFooter(users: List<User>, onExploreClick: () -> Unit) {
+fun UniverseCardFooter(onExploreClick: () -> Unit) {
     Row(
         modifier = Modifier
             .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
+        horizontalArrangement = Arrangement.End
     ) {
-        UniverseCardStackedAvatars(users)
-
         DexButton(
             onClick = { onExploreClick() },
             colors = ButtonDefaults.buttonColors(containerColor = Color.White),
-            modifier = Modifier
-                .fillMaxWidth(0.8f)
-                .height(35.dp)
+            modifier = Modifier.fillMaxWidth(0.8f)
         ) { Text("Explore", color = Color.Black) }
     }
 }
